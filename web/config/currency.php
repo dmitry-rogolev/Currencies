225aa1735698d51bf7c3649201a61eeb1d7483c1
Dmitry <work.drogolev@internet.ru>
2022-08-02 13:42:47 +0700

    Настройка конфига

---
+++ web/config/currency.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    "source" => env("CURRENCY_SOURCE", "http://www.cbr.ru/scripts/XML_daily.asp"), 
+
+    "default_interval" => env("CURRENCY_UPLOAD_DEFAULT_INTERVAL", 3600), 
+
+];
